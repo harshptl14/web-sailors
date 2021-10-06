61af26bb255b833d71f00adde8dd830a6dcbc2d3
harshptl14 <harshptl714@gmail.com>
2021-10-06 18:26:57 +0530

    Initian commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-transformer-remark",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: `Web Sailors`,
+    description: `A collection of web development tutorials and resources.`,
+    copyright: `Copyright © 2020 Web Sailors`,
+    contact: 'me@web-sailors.com'
+  },
 }
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const about = () => {
+  return (
+      <Layout>
+    <div>
+      <h1>About Page</h1>
+      <p>This is just a about page, nothing more.</p>
+    </div>
+    </Layout>
+  )
+}
+
+export default about
